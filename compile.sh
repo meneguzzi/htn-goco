fec276a6318e204755a258bfe97b539965dc8954
Felipe Meneguzzi <meneguzzi@users.noreply.github.com>
2015-08-01 01:36:43 +0000

    Added code from JSHOP implementation of GoCo.

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ $# -lt 3 ]
+	then echo "Usage ${0} <domain-name> <problem-name> <gui|nogui> [a|<0..n>]"
+	exit 1
+else
+	echo $CLASSPATH
+	export CLASSPATH=../jshop2/antlr.jar:../jshop2/JSHOP2.jar:.
+	java JSHOP2.InternalDomain ${1}.jshop
+	java JSHOP2.InternalDomain -r${4} ${2}.jshop
+	javac *.java
+	if [ $3 = "gui" ]; then
+		java gui $2
+	else
+		java run $2
+	fi
+	rm *.class
+	rm ${1}.java ${2}.java
+fi
