68e60d67b054df030e914f89e6fb7ca453bf46f4
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2017-04-11 16:06:13 -0300

    Updated compilation script

--- compile.sh
+++ compile.sh
@@ -3,8 +3,9 @@ if [ $# -lt 3 ]
 	then echo "Usage ${0} <domain-name> <problem-name> <gui|nogui> [a|<0..n>]"
 	exit 1
 else
-	# echo $CLASSPATH
+	echo $CLASSPATH
 	export CLASSPATH=./jshop2/antlr.jar:./jshop2/JSHOP2.jar:.
+	echo $CLASSPATH
 	java -cp $CLASSPATH JSHOP2.InternalDomain ${1}.jshop
 	java -cp $CLASSPATH JSHOP2.InternalDomain -r${4} ${2}.jshop
 	javac *.java
