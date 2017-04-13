8d5827d0bb180c2cee2879fe6d17fe4983c32e8e
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2017-04-13 18:11:13 -0300

    Added test problems to run remotely.

--- compile.sh
+++ compile.sh
@@ -4,15 +4,35 @@ if [ $# -lt 3 ]
 	exit 1
 else
 	# echo $CLASSPATH
-	export CLASSPATH=./jshop2/antlr.jar:./jshop2/JSHOP2.jar:.
+	export CLASSPATH=./jshop2/antlr.jar:./jshop2/bin.build/JSHOP2.jar:.
+	export PARS="-Xms8g -Xmx12g"
 	# echo $CLASSPATH
-	java -cp $CLASSPATH JSHOP2.InternalDomain ${1}.jshop
-	java -cp $CLASSPATH JSHOP2.InternalDomain -r${4} ${2}.jshop
+	# DOMAIN=${1}
+# 	PROBLEM=${2}
+#
+# 	if [ -e ${1}.jshop ]
+# 	then
+# 		DOMAIN=${1}.jshop
+# 	elif [ -e ${1}.ujshop ]
+# 	then
+# 		DOMAIN=${1}.ujshop
+# 	fi
+#
+# 	if [ -e ${2}.jshop ]
+# 	then
+# 		PROBLEM=${2}.jshop
+# 	elif [ -e ${2}.ujshop ]
+# 	then
+# 		PROBLEM=${2}.ujshop
+# 	fi
+	
+	java -cp $CLASSPATH JSHOP2.InternalDomain ${1}.jshop #${DOMAIN}
+	java -cp $CLASSPATH JSHOP2.InternalDomain -r${4} ${2}.jshop #${PROBLEM}
 	javac *.java
 	if [ $3 = "gui" ]; then
-		java gui $2
+		java ${PARS} gui $2
 	else
-		java run $2
+		java ${PARS} run $2
 	fi
 	rm *.class
 	rm ${1}.java ${2}.java ${1}.txt
